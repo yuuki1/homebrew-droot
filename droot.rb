60db8d376cf4f576f9de201b56d6af9ab8eb6ec8
y_uuki <yuki.tsubo@gmail.com>
2016-02-29 20:46:11 +0900

    droot v0.3.1

--- droot.rb
+++ droot.rb
@@ -1,14 +1,14 @@
 class Droot < Formula
-  droot_version = '0.3.0'
+  droot_version = '0.3.1'
   homepage 'https://github.com/yuuki1/droot'
   version droot_version
 
   if Hardware.is_64_bit?
     url "https://github.com/yuuki1/droot/releases/download/v#{droot_version}/droot_darwin_amd64.zip"
-    sha256 '8b572f8dff888ab7a2da00a5ff96bce5a64da2c5e0d0e8f33a13e058783ba93a'
+    sha256 'f2548b8c38e9824598c516e21dea645153c8664bb3513dd9a2ac41e9fd857d11'
   else
     url "https://github.com/yuuki1/droot/releases/download/v#{droot_version}/droot_darwin_386.zip"
-    sha256 '8a96170fb9dc3604d6d805df87aca75c49fabd0d00929c24e3dd730d6cb2bf24'
+    sha256 'e19b75df7d4038d60b2afab35d1ce54df95c41d0cb4c7094826eca051aaf2744'
   end
 
   head do
