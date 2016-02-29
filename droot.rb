93fcd266c3a9a9b069377a04c53a240681436bed
y_uuki <yuki.tsubo@gmail.com>
2016-02-29 20:49:24 +0900

    Fix sha256

--- droot.rb
+++ droot.rb
@@ -5,7 +5,7 @@ class Droot < Formula
 
   if Hardware.is_64_bit?
     url "https://github.com/yuuki1/droot/releases/download/v#{droot_version}/droot_darwin_amd64.zip"
-    sha256 'f2548b8c38e9824598c516e21dea645153c8664bb3513dd9a2ac41e9fd857d11'
+    sha256 'ebad2b191c70d8a8af1776bfca0f4d3bc52573a7546423ba2c454c025befdea2'
   else
     url "https://github.com/yuuki1/droot/releases/download/v#{droot_version}/droot_darwin_386.zip"
     sha256 'e19b75df7d4038d60b2afab35d1ce54df95c41d0cb4c7094826eca051aaf2744'
